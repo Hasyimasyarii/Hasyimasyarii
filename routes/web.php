f857d1d6a57b06482c5f68b429bc9f34d06dfba7
Hasyim Asyari <arikseno5@gmail.com>
2023-08-01 11:15:07 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('login', [LoginController::class, 'authenticate']);
+Route::post('logout', [LoginController::class, 'logout']);
