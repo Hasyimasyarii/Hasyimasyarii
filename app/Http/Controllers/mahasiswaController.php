0f350cb7070dea3c42533d07a2e28a5350de59dc
Hasyim Asyari <arikseno5@gmail.com>
2023-08-01 16:45:14 +0700

    memperbaiki error pada blade

--- app/Http/Controllers/mahasiswaController.php
+++ app/Http/Controllers/mahasiswaController.php
@@ -10,8 +10,6 @@ class mahasiswaController extends Controller
 {
     /**
      * Display a listing of the resource.
-     * 
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
@@ -20,8 +18,6 @@ class mahasiswaController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
